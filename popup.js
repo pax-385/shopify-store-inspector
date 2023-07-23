bfc11c0f67e0e119e65ab40474284c775496cc23
Tomas Hermansky <tomas.hermansky@gmail.com>
2023-07-23 22:11:47 +0200

    added spinner while fetching the data

--- popup.js
+++ popup.js
@@ -4,15 +4,21 @@ document.addEventListener('DOMContentLoaded', function() {
             cmd: 'FETCH_JSON', 
             url: new URL(tabs[0].url).origin + '/'
         }, response => {
-            if (response.error !== undefined) {
-                document.getElementById('error').innerText = "This website doesn't run on Shopify";
-            } else {
-                for (let [key, value] of Object.entries(response)) {
-                    let p = document.createElement('p');
-                    p.innerText = `${key}: ${value}`;
-                    document.body.appendChild(p);
+            // Extend the loading time by 1s
+            setTimeout(function() {
+                document.getElementById('loader').style.display = 'none'; // Hide the loader
+
+                if (response.error !== undefined) {
+                    document.getElementById('error').innerText = "This website doesn't run on Shopify";
+                    document.getElementById('error').style.display = 'block';
+                } else {
+                    for (let [key, value] of Object.entries(response)) {
+                        let p = document.createElement('p');
+                        p.innerText = `${key}: ${value}`;
+                        document.getElementById('data').appendChild(p);
+                    }
                 }
-            }
+            }, 1000);
         });
     });
 });
